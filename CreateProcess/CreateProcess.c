30d21d47b0368e6726c5073308b92999c5625957
Matt Warner <mlwarner632@gmail.com>
2014-10-27 13:38:47 -0400

    CreateProcess 	- Added messaging capabilities TellerInterface 	- Added a message structure

--- CreateProcess/CreateProcess.c
+++ CreateProcess/CreateProcess.c
@@ -14,10 +14,10 @@
 // Starts the Teller Server process
 static pid_t StartTellerTask()
 {
-	pid_t tellerTask ;		// process ID of the teller server task that we start
+	pid_t tellerTask ;					// process ID of the teller server task that we start
 	char *teller_argv[] = {tellerProcessFileName, NULL} ;	// the first argument must be the name of the program we are running
-	struct inheritance childInheritance ;					// defines the child's privileges
-	childInheritance.flags = 0 ;							// tell the child process to use the same privileges as the parent
+	struct inheritance childInheritance ;			// defines the child's privileges
+	childInheritance.flags = 0 ;				// tell the child process to use the same privileges as the parent
 
 	// Start the teller server. Since the second parameter is 0 the third parameter for the file descriptor
 	// mapping can be NULL. The fifth parameter is NULL meaning that the child inherits the parents environment variables.
@@ -44,13 +44,62 @@ static void CloseTellerSemaphore( sem_t *ptrSemaphore)
 	assert( status == 0 ) ;
 }
 
+// returns connection ID
+static int OpenConnection( const char *channelName )
+{
+	int connectionID = name_open( channelName, 0 ) ;
 
+	assert( connectionID != -1 ) ;
+	return connectionID ;
+}
+
+static void CloseConnection( int connectionID )
+{
+	name_close( connectionID ) ;
+}
+
+
+static void tellerClientDriver( int connectionID )
+{
+	int counter ;
+	int status ;
+	tellerRequest oneRequest ;
+	tellerRequest requestResponse ;
+	uint64_t startClock ;
+	uint64_t stopClock ;
+
+	// this will tell you how long it takes for this particular system clock to wrap around.
+	// If you exceed that time then your timing measurement will be wildly inaccurate.
+	//printf( "seconds per clock wrap %llu\n", (~(uint64_t)0) /SYSPAGE_ENTRY(qtime) -> cycles_per_sec ) ;
+
+	oneRequest.hdr.type = 0 ;
+	oneRequest.hdr.subtype = 0 ;
+
+	startClock = ClockCycles() ;
+	// simulate bank simulator
+	for ( counter = 0 ; counter < 10000 ; counter++ )
+	{
+		status = MsgSend( connectionID, &oneRequest, sizeof(oneRequest), &requestResponse, sizeof(requestResponse) ) ;
+		assert( status != -1 ) ;
+	}
+	stopClock = ClockCycles() ;
+	printf( "%llu clock cycles at %llu cycles per second\n", stopClock - startClock,SYSPAGE_ENTRY(qtime) -> cycles_per_sec  ) ;
+}
 
 int main(int argc, char *argv[])
 {
+	int connectionID = OpenConnection( serverNamedChannel ) ;
 	sem_t *tellerSemaphore = CreateTellerSemaphore() ;	// Create the teller semaphore by name
 	pid_t tellerTask = StartTellerTask() ;		// process ID of the teller server task that we start
+
 	printf( "semaphore id: %lx   pid: %d\n", (unsigned long)tellerSemaphore, tellerTask) ;
+	
+	printf( "Connected to Server\n" ) ;
+	tellerClientDriver( connectionID ) ;
+	
+	printf( "Client exiting\n" ) ;
+	CloseConnection( connectionID ) ;
 	CloseTellerSemaphore(tellerSemaphore) ;
+
 	return EXIT_SUCCESS;
 }
